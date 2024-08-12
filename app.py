4baa15d4e47246951f5c22556ea064c41ff0d492
windskyshao <windskyshao@gmail.com>
2024-08-12 20:57:09 +0800

    Update app.py

--- app.py
+++ app.py
@@ -255,7 +255,7 @@ def handle_message(event):
                         # print(dataList[i][k])
                         look_stock_price(dataList[i][k]['favorite_stock'], dataList[i][k]['condition'], dataList[i][k]['price'], dataList[i][k]['userID'])
                         # look_stock_price(stock='2002', condition='>', price=31)
-            schedule.every(5).seconds.do(job).tag('daily-tasks-stock'+uid,'second') #每10秒執行一次
+            schedule.every(10).seconds.do(job).tag('daily-tasks-stock'+uid,'second') #每10秒執行一次
             #schedule.every().hour.do(job) #每小時執行一次
             #schedule.every().day.at("17:19").do(job) #每天9點30執行一次
             #schedule.every().monday.do(job) #每週一執行一次
